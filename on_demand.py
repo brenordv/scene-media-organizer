b8f764a5f78bc23e2d525ad5002e6bbb5c166f0a
Breno RdV <brenordv@users.noreply.github.com>
2025-09-07 01:01:40 -0400

    adapted path handling in on_demand.py to support local execution and dynamic base folder resolution

--- on_demand.py
+++ on_demand.py
@@ -10,9 +10,19 @@ from src.data.notification_repository import NotificationRepository
 from src.data.work_queue_manager import WorkQueueManager
 
 _work_queue_manager = WorkQueueManager()
+_watch_folder = os.environ.get('WATCH_FOLDER')
 _movies_base_folder = os.environ.get('MOVIES_BASE_FOLDER')
 _series_base_folder = os.environ.get('SERIES_BASE_FOLDER')
 _notification_agent = NotificationRepository(client_id="smo-watchdog-notification-sender")
+
+
+from pathlib import Path
+
+old_path = Path("/old/root/folder/subdir/file.txt")
+old_root = Path("/old/root/folder")
+new_root = Path("/new/root/folder")
+
+new_path = new_root / old_path.relative_to(old_root)
 
 
 def on_demand_batch():
@@ -29,8 +39,33 @@ def on_demand_batch():
         activity_tracker.info("No items found in the queue. Nothing to do here.")
         return
 
-    process_batch(batch, batch_id)
+    activity_tracker.info("Adapting paths for running locally...")
+    for item in batch:
+        activity_tracker.debug(f"Old full_path: {item['full_path']}")
+        full_path = Path(_watch_folder) / Path(item['full_path']).relative_to(Path("/watch"))
+        item['full_path'] = str(full_path.resolve())
+        activity_tracker.debug(f"New full_path: {item['full_path']}")
+        item["parent"] = str(full_path.parent)
 
+        if item["target_path"] is None:
+            activity_tracker.debug(f"No target path found for item [{item['id']}].")
+            continue
+
+        activity_tracker.debug(f"Old target_path: {item['target_path']}")
+
+        if "movies" in item['target_path'].lower():
+            target_path = Path(_movies_base_folder) / Path(item['target_path']).relative_to(Path("/movies"))
+            item['target_path'] = str(target_path.resolve())
+            activity_tracker.debug(f"New target_path: {item['target_path']}")
+            continue
+
+        target_path = Path(_series_base_folder) / Path(item['target_path']).relative_to(Path("/series"))
+        item['target_path'] = str(target_path.resolve())
+        activity_tracker.debug(f"New target_path: {item['target_path']}")
+
+    activity_tracker.info(f"Processing batch with id [{batch_id}]...")
+
+    process_batch(batch, batch_id)
 
 
 def on_demand_process_missing():
